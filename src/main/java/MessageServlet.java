d6dff5e4654b718f41b5d26bd6e01a1df425a70a
sosuisen <hidekazu.kubota@gmail.com>
2023-04-08 14:44:21 +0900

    first commit

---
+++ src/main/java/MessageServlet.java
@@ -0,0 +1,75 @@
+
+import java.io.IOException;
+import java.util.ArrayList;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+/**
+ * Servlet implementation class MessageServlet
+ */
+@WebServlet(urlPatterns = { "/message", "/clear" })
+public class MessageServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#HttpServlet()
+	 */
+	public MessageServlet() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		HttpSession session = request.getSession();
+
+		if (session.getAttribute("history") == null) {
+			session.setAttribute("history", new ArrayList<String>());
+		}
+
+		// 発展課題 4-1a
+		// ArrayListのクリア
+		String path = request.getServletPath();
+		if (path.equals("/clear")) {
+			ArrayList<String> list = (ArrayList<String>) session.getAttribute("history");
+			list.clear();
+		}
+
+		request.getRequestDispatcher("/WEB-INF/message.jsp").forward(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		request.setCharacterEncoding("UTF-8");
+
+		String message = request.getParameter("message");
+		HttpSession session = request.getSession();
+
+		// 発展課題 4-1b。doPostでもなくなったセッションの初期化が必要となる。
+
+		if (session.getAttribute("history") == null) {
+			session.setAttribute("history", new ArrayList<String>());
+		}
+		session.setMaxInactiveInterval(10);
+
+		if (session.getAttribute("history") != null) {
+			ArrayList<String> list = (ArrayList<String>) session.getAttribute("history");
+			list.add(message);
+		}
+		request.getRequestDispatcher(
+				"/WEB-INF/message.jsp").forward(request, response);
+
+	}
+
+}
